97c7efde84b585e1b40e8bb617d15c2f679fcffa
parkjubro <juhyeong97@gmail.com>
2022-03-09 20:07:33 +0900

    BOJ_미해결_박주형

---
+++ 1주차/BOJ_2563/BOJ_2563_박주형.py
@@ -0,0 +1,9 @@
+paper = [[0] * 100 for _ in range(100)]
+
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    for i in range(x, x + 10):
+        for j in range(y, y + 10):
+            paper[i][j] = 1
+
+print(sum(sum(line) for line in paper))
---
+++ 1주차/BOJ_2669/BOJ_2669_박주형.py
@@ -0,0 +1,8 @@
+paper = [[0] * 100 for _ in range(100)]
+
+for _ in range(4):
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(x1, x2):
+        for j in range(y1, y2):
+            paper[i][j] = 1
+print(sum(sum(line) for line in paper))
