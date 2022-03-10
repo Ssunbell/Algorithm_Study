206d0f7025f066225b8ccb6fb0445f8af1cf67d2
wldntls <sddyy1004@naver.com>
2022-03-11 00:44:19 +0900

    BOJ_1032_신지우_fix

--- 1주차/BOJ_1032/BOJ_1032_신지우.py
+++ 1주차/BOJ_1032/BOJ_1032_신지우.py
@@ -1,10 +1,11 @@
-N = int(input())
-line = list(input())
+n = int(input())
+file_name = list(input())
 
-for i in range(N-1):
-    other = input()
-    print(other)
-    for j in range(len(line)):
-        if line[j] != other[j]:
-            line[j] ='?'
-print(''.join(line))
+for i in range(n-1):
+    file_names = list(input())
+    for j in range(len(file_name)):
+        if file_name[j] != file_names[j]:
+            file_name[j] = '?'
+
+for i in file_name:
+    print(i, end="")
