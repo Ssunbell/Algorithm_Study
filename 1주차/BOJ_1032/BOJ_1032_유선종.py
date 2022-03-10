d83f8b2a9023c050a5d086ab8fbbaddede38eb82
Trailblazer-Yoo <yousun9591@naver.com>
2022-03-10 12:29:31 +0900

    BOJ_1032_유선종_fix

--- 1주차/BOJ_1032/BOJ_1032_유선종.py
+++ 1주차/BOJ_1032/BOJ_1032_유선종.py
@@ -2,11 +2,15 @@ n = int(input())
 if n == 1:
     print(input())
 else:
-    space = [input() for i in range(n)]
-    for i, text in enumerate(space):
-        for j, text2 in enumerate(space):
-            if i < j:
-                for k in range(len(text)):
-                    if text[k] != text2[k]:
-                        text[k] = '?'
-                        
+    space = [list(input()) for i in range(n)]
+    print(space)
+    text_len = len(space[0])
+    print(text_len)
+    total = []
+    for col in range(text_len):
+        intersection = set([space[row][col] for row in range(n)])
+        if len(intersection) > 1:
+            total.append('?')
+        else:
+            total.append(space[0][col])
+    print(''.join(total))
