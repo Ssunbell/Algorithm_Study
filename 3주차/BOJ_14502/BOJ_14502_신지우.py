3af3ff60dcd6a20466c94072cf69b14497b44f06
wldntls <sddyy1004@naver.com>
2022-03-31 00:36:54 +0900

    BOJ_14502_신지우_fix

--- 3주차/BOJ_14502/BOJ_14502_신지우.py
+++ 3주차/BOJ_14502/BOJ_14502_신지우.py
@@ -17,10 +17,6 @@ for i in range(n-1, -1, -1):
     if day_list[i] + i > n: 
         dp[i] = dp[i+1] 
     else: 
-        print(dp[i+1]) 
-        print(day_list[i],'ㅓㅈ에') 
-        print(dp[i+day_list[i]],'wad') 
-        print(pay_list[i]+dp[i+day_list[i]],'ws')
         dp[i] = max(dp[i+1], pay_list[i]+dp[i+day_list[i]]) 
 
 print(dp[0])
