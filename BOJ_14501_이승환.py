fb853ed9e0d6909a758e20db960332209698ebe9
lee_seunghwan <tmdghks304@gmail.com>
2022-04-02 01:24:08 +0900

    14501번 문제풀이

--- BOJ_14501_이승환.py
+++ BOJ_14501_이승환.py
@@ -1,27 +1,25 @@
+# 우선 회사에 없어서 상담을 하지 못하는 경우는 수익을 0으로 계산
+# p을 i일차의 상담기간이라고 할 경우
+# i+p, i+p+1, i+p+2.... N일차 까지의 수익 중 가장 큰 수익을 선택
+# i일차의 최대 수익 = i일차의 수익 + i일차의 상담기간 이후 수익중 최대수익
+
 N = int(input())
 
-counseling = [list(map(int,input().split())) for _ in range(N)]
+s = [list(map(int,input().split())) for _ in range(N)]
+# 날짜 계산의 용이성을 위해 더미 추가
+dummy = [[0,0]]
+schedule = dummy + s
 
-# 각 일차별 상담을 했을 시 상담 가능한 모든 날 구학.
-counsel_day_aff = []
+# 처음에는 N+1로 길이를 설정했는데, 런타임에러발생,
+# 이유는 상담일자가 최대 5일이고, i와 i+p를 비교해야하기때문.
+# 즉, N+5까지 비교할 수 있기 때문에 N+5로 설정해야함.
+# vscode상으로는 문제가 없었지만 백준 내부에서 오류가 있는듯.
+income = [0 for _ in range(N+5)]
 
-for j in range(N):
-    if counseling[j][0] <= N-j: # N+1일에 회사에 없어서 상담을 못하는 상담일정을 제외
-        no_cp = j + counseling[j][0] # j일차에 상담일 + j = 이날까지 상담 불가능
-        cd = []
-        cd.append(j)
-        for i in range(j+1,N):
-            if i >= no_cp and counseling[i][0] <= N-i: # 상담불가기간이 아니고 회사에있는날
-                cd.append(i)
-                no_cp = i + counseling[i][0] # 새로운 상담불가기간 생성
-        counsel_day_aff.append(cd)
+for i in range(N,0,-1):
+    if schedule[i][0] > N+1-i:
+        income[i] = 0
+    else:
+        income[i] = schedule[i][1] + max(income[i+schedule[i][0]:])
 
-sal = []
-for i in counsel_day_aff:
-    sum = 0
-    for j in i:
-        sum += counseling[j][1]
-    sal.append(sum)
-
-print(max(sal))
-
+print(max(income))
