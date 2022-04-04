d4abfeb65a15a187b1c0570782276875bc7d6c78
parkjubro <juhyeong97@gmail.com>
2022-04-04 22:50:02 +0900

    BOJ_10828_박주형

---
+++ 4주차/BOJ_10828/BOJ_10828_박주형.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+program = [list(input().split()) for i in range(n)]
+stack = []
+
+for i in range(n):
+    if program[i][0] == 'push':
+        stack.append(program[i][1])
+    if program[i][0] == 'pop':
+        if not stack:
+            print(int(-1))
+        else:
+            pop = stack.pop()
+            print(pop)
+    if program[i][0] == 'size':
+        print(len(stack))
+    if program[i][0] == 'empty':
+        if len(stack) == 0:
+            print(int(1))
+        else:
+            print(int(0))
+    if program[i][0] == 'top':
+        if len(stack) == 0:
+            print(int(-1))
+        else:
+            top = stack[-1]
+            print(top)
+
+# 시간초과 해결하기
