aa57d9258c2ae0ea47c72dd445e3d2489cfbffa6
lee_seunghwan <tmdghks304@gmail.com>
2022-04-13 22:52:01 +0900

    이승환 문제풀이

--- 5주차/BOJ_14502/BOJ_14502_이승환.py
+++ 5주차/BOJ_14502/BOJ_14502_이승환.py
@@ -1,36 +1,67 @@
-# 행렬 정보를 N x M 이중리스트에 저장
-# 1단계 : 벽을 세울 수 있는 모든 경우의 수에 벽을 세움.
-# 2단계 : 바이러스가 있는 위치를 저장, BFS로 바이러스가 퍼질 수 있는 구역을 탐색.
-# 3단계 : 최종 결과 안전지역의 수를 저장, 가장 높은 값을 출력
+# n,m = map(int,input().split())
+# lab = [list(input().split()) for _ in range(n)]
 
-# 행렬 정보 받기
-n,m = map(int,input().split())
-lab = [list(input().split()) for _ in range(n)]
+def zero_index(arr):
+    result = []
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] == 0:
+                result.append([i,j])
+    return result
 
-# 1단계, 연구실 이중리스트를 받아 벽을 세우고, 그 정보(리스트)를 반환
-# 벽 세우는 아이디어
-# 0 원소중에서 3개를 뽑는 조합을 구함.
-# 해당 조합에서 사용하는 값은 0 원소의 인덱스값.
+def combination(arr, r):
+    # 1.
+    arr = sorted(arr)
+    used = [0 for _ in range(len(arr))]
+    result = []
+    # 2.
+    def generate(chosen):
+        if len(chosen) == r:
+            result.append(chosen[:])
+            return
+    	# 3.
+        start = arr.index(chosen[-1]) + 1 if chosen else 0
+        for nxt in range(start, len(arr)):
+            if used[nxt] == 0 and (nxt == 0 or arr[nxt-1] != arr[nxt] or used[nxt-1]):
+                chosen.append(arr[nxt])
+                used[nxt] = 1
+                generate(chosen)
+                chosen.pop()
+                used[nxt] = 0
+    generate([])
+    return result
 
-# 조합을 구하는 함수
-def comb_zero(list):
-    pass
+def make_wall(arr):
+    result = []
+    for i in range(len(arr)):
+        lab_copy = [item[:] for item in lab]
+        for j in range(len(arr[i])):
+            lab_copy[arr[i][j][0]][arr[i][j][1]] = 1
+        result.append([item[:] for item in lab_copy])
+    return(result)
 
-# 구한 조합을 가지고 벽을 세우는 함수
-def wall(list):
-    pass
+# 2차원배열을 받아서 2차원배열로 출력
+# 바이러스가 퍼지는 것을 구현해야함.
+def virus(arr):
+    return arr
 
-# 2단계, 리스트를 받아 최종 안전지역의 수를 반환
-# 1. 2가 있는 인덱스를 모두 리스트에 저장
-# 2. 첫번째 2에서 BFS실시, 인접 노드가 1일경우 멈춤, 인접 노드가 0일경우 추가 탐색
-# 2-1. 2 노드를 큐에 삽입하고 방문처리
-# 2-2. 2 주변노드를 큐에 삽입하고 방문처리
-# 3. 모든 인접노드가 1인경우 탐색종료, 탐색하지 않은 2에서 재탐색 실시.
-# 주의 : 탐색중에 2을 만날 경우 0과 같이 판단하되, 2가 저장된 리스트에서 삭제
-def virus(list):
-    pass
+def find_zero(arr):
+    sum_zero = 0
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            if arr[i][j] == 0:
+                sum_zero += 1
+    return sum_zero
 
-# 3단계, 안전지역의 수를 카운트
-# 0의 개수를 세는 함수.
-def count_zero(list):
-    pass
+lab = [[2,0,0],[0,0,1]]
+
+zero_idx = zero_index(lab)
+comb_zero = combination(zero_idx,3)
+wall_lab = make_wall(comb_zero)
+lab_result = []
+for i in wall_lab:
+    lst = virus(i)
+    lab_result.append(find_zero(lst))
+
+print(lab_result)
+
--- 5주차/BOJ_14889/BOJ_14889_이승환.py
+++ 5주차/BOJ_14889/BOJ_14889_이승환.py
@@ -1,3 +1,45 @@
 n = int(input())
 s = [list(map(int,input().split())) for _ in range(n)]
 
+def sum_capa(lst):
+    capa = 0
+    for i in lst:
+        for j in lst:
+            capa += s[i][j] + s[j][i]
+    return int(capa/2)
+
+team_mate = [i for i in range(n)]
+
+def combination(arr, r):
+    # 1.
+    arr = sorted(arr)
+    used = [0 for _ in range(len(arr))]
+    result = []
+    # 2.
+    def generate(chosen):
+        if len(chosen) == r:
+            if 0 not in chosen:
+                return
+            result.append(chosen[:])
+            return
+    	# 3.
+        start = arr.index(chosen[-1]) + 1 if chosen else 0
+        for nxt in range(start, len(arr)):
+            if used[nxt] == 0 and (nxt == 0 or arr[nxt-1] != arr[nxt] or used[nxt-1]):
+                chosen.append(arr[nxt])
+                used[nxt] = 1
+                generate(chosen)
+                chosen.pop()
+                used[nxt] = 0
+    generate([])
+    return result
+k = combination(team_mate,int(len(team_mate)/2))
+
+result = []
+
+for lst in k:
+    lst2 = [i for i in range(n) if i not in lst]
+    result.append(abs(sum_capa(lst) - sum_capa(lst2)))
+
+print(min(result))
+
