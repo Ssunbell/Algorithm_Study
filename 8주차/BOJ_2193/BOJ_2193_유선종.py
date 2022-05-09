2c7b72ec9973c673d15b503da7c6c3ad7f641b19
Trailblazer-Yoo <yousun9591@naver.com>
2022-05-10 01:40:07 +0900

    BOJ_2193_유선종

--- 8주차/BOJ_2193/BOJ_2193_유선종.py
+++ 8주차/BOJ_2193/BOJ_2193_유선종.py
@@ -1,10 +1,10 @@
-# n = int(input())
-# dp = [0, 1, 2] + [0]* (n-1)
+n = int(input())
+dp = [0, 1, 2] + [0]* (n-1)
 
-# for i in range(3, n + 1):
-#     dp[i] = dp[i-2] + dp[i-1]
+for i in range(3, n + 1):
+    dp[i] = dp[i-2] + dp[i-1]
 
-# print(dp[n])
+print(dp[n])
 
 # 메모리 오류 남
 
