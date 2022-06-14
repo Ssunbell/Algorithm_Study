7d0afa229a98c680b6b921ea8c830fb48ccb9f15
lee_seunghwan <tmdghks304@gmail.com>
2022-06-15 03:44:32 +0900

    1966 풀이

--- 11주차/1966/BOJ_1966_이승환.py
+++ 11주차/1966/BOJ_1966_이승환.py
@@ -1,8 +1,32 @@
 import sys
 from collections import deque
 
-input_s = lambda : sys.stdin.readline().strip()
-n, idx = map(int,input_s().split())
-pri = list(map(int,input_s().split()))
+t = int(input())
 
-q = deque()
+for _ in range(t):
+    input_s = lambda : sys.stdin.readline().strip()
+    n, idx = map(int,input_s().split())
+    pri = list(map(int,input_s().split()))
+
+    q = deque()
+
+    for i in range(n):
+        q.append((pri[i],i))
+
+    check = False
+    pnum = 0
+    while q:
+        curr = q.popleft()
+        for j in range(len(q)):
+            if q[j][0] > curr[0]:
+                q.append(curr)
+                check = True
+                break
+            else:
+                check = False
+        if check == False:
+            pnum += 1
+            if curr[1] == idx:
+                break
+    print(pnum)
+
