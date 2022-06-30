4d540f30b70e83150fac09193d3a8fb5f00bfee2
parkjubro <juhyeong97@gmail.com>
2022-06-30 20:41:46 +0900

    박주형 문제풀이

---
+++ 13주차/BOJ_4963/BOJ_4963_박주형.py
@@ -0,0 +1,31 @@
+import sys
+sys.setrecursionlimit(5000000)
+ 
+def dfs(x, y):
+    if 0 <= x < h and 0 <= y < w:
+        if graph[x][y] == 1:
+            graph[x][y] = 2
+            dfs(x-1, y-1)
+            dfs(x-1, y)
+            dfs(x-1, y+1)
+            dfs(x, y-1)
+            dfs(x, y+1)
+            dfs(x+1, y-1)
+            dfs(x+1, y)
+            dfs(x+1, y+1)
+            return True
+        return False
+ 
+while True:
+    w, h = map(int, input().split())
+    if not w and not h:
+        break
+    graph = [list(map(int, input().split())) for _ in range(h)]
+    cnt = 0
+ 
+    for i in range(h):
+        for j in range(w):
+            if graph[i][j] == 1:
+                dfs(i, j)
+                cnt += 1
+    print(cnt)
