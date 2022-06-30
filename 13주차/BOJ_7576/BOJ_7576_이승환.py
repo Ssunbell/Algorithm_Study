14c126a77f80c1db30c36fb12d1527eb30797669
lee_seunghwan <tmdghks304@gmail.com>
2022-06-30 22:47:32 +0900

    7576번 수정

--- 13주차/BOJ_7576/BOJ_7576_이승환.py
+++ 13주차/BOJ_7576/BOJ_7576_이승환.py
@@ -8,16 +8,12 @@ box = []
 for _ in range(n):
     box.append(list(map(int,input_s().split())))
 
-unripe = deque()
 q = deque()
-visited = deque()
 depth_chart = dict()
 
 for i in range(n):
     for j in range(m):
-        if box[i][j] == 0:
-            unripe.append((i,j))
-        elif box[i][j] == 1:
+        if box[i][j] == 1:
             q.append((i,j))
             depth_chart[(i,j)] = 0
 
@@ -26,18 +22,24 @@ dj = [0,1,0,-1]
 
 while q:
     curr = q.popleft()
-    visited.append(curr)
 
     for i in range(4):
         next = (curr[0]+di[i],curr[1]+dj[i])
         if next[0] < 0 or next[1] < 0 or next[0] >= n or next[1] >= m:
             continue
-        if next not in q and next not in visited and box[next[0]][next[1]] != -1:
+        if box[next[0]][next[1]] == -1:
+            continue
+        if box[next[0]][next[1]] == 0:
+            box[next[0]][next[1]] = 1
             q.append(next)
-            unripe.remove(next)
             depth_chart[next] = depth_chart[curr] + 1
-
-if unripe:
+check = False
+for i in range(n):
+    for j in range(m):
+        if box[i][j] == 0:
+            check = True
+            break
+if check:
     result = -1
 else:
     result = max(depth_chart.values())
