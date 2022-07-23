2811c42d9e8b6dd5b5e0c7f4f44ba62eac242dd9
Trailblazer-Yoo <yousun9591@naver.com>
2022-07-23 21:02:25 +0900

    PRO_방금그곡_유선종

---
+++ 17주차/PRO_방금그곡/PRO_방금그곡_유선종.py
@@ -0,0 +1,46 @@
+
+def solution(m, musicinfos):
+    melody_dict = {'a':'C#', 'b':'F#', 'c':'D#', 'd':'G#', 'e':'A#', 'f':'F',
+                       'g':'C', 'h':'G', 'i':'D', 'j':'A', 'k':'E', 'l':'B'}
+    
+    for node, re in melody_dict.items():
+        m = m.replace(re, node)
+    print(m)
+    
+    melody_list = []
+    for row in musicinfos:
+        # 재생시간 구하기
+        row = row.split(',')
+        tmp_time = row[0].split(':')
+        h1, m1 = int(tmp_time[0]), int(tmp_time[1])
+        tmp_time = row[1].split(':')
+        h2, m2 = int(tmp_time[0]), int(tmp_time[1])
+        cycle = ((h2 - h1) * 60 + m2 - m1)
+        
+        # 악보에 사용되는 음 치환
+        melody = row[-1]
+        for node, re in melody_dict.items():
+            melody = melody.replace(re, node)
+            
+        tmp_mel = ''
+        for i in range(cycle):
+            tmp_mel += melody[i % len(melody)]
+        melody_list.append([cycle, tmp_mel])
+    
+    print(melody_list)
+    answer = []
+    for i, mel in enumerate(melody_list):
+        if m in mel[1]:
+            answer.append([mel[0],musicinfos[i].split(',')[2]])
+    print(answer)
+    if len(answer) == 0:
+        return "(None)"
+    elif len(answer) == 1:
+        return answer[0][1]
+    else:
+        answer = sorted(answer, key=lambda x: -x[0])
+        return answer[0][1]
+
+m = "ABCDEFG"
+musicinfos = ["12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"]
+print(solution(m, musicinfos))
