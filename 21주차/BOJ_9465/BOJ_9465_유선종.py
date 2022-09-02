a738d8ccb16a0a3da92d2b284564fb9ea5803206
Trailblazer-Yoo <yousun9591@naver.com>
2022-09-02 21:00:16 +0900

    fix

--- 21주차/BOJ_9465/BOJ_9465_유선종.py
+++ 21주차/BOJ_9465/BOJ_9465_유선종.py
@@ -12,9 +12,13 @@ for _ in range(t):
     dp[0][0] = sticker[0][0]
     dp[1][0] = sticker[1][0]
     
-    for i in range(1, n):
-        dp[0][i] = max(dp[1][i-1] + sticker[0][i], dp[1][i-2] + sticker[0][i])
-        dp[1][i] = max(dp[0][i-1] + sticker[1][i], dp[0][i-2] + sticker[1][i])
+    # 1번째
+    dp[0][1] = dp[1][0] + sticker[0][1]
+    dp[1][1] = dp[0][0] + sticker[1][1]
+    if n > 2:
+        for i in range(2, n):
+            dp[0][i] = max(dp[1][i-1] + sticker[0][i], dp[1][i-2] + sticker[0][i])
+            dp[1][i] = max(dp[0][i-1] + sticker[1][i], dp[0][i-2] + sticker[1][i])
     print(max(dp[0][n-1], dp[1][n-1]))
     
     
