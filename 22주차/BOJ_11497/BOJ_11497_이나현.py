c8d66bcaaac3e2de73ddd66b47e116e92c82472e
nahyunlee <awsed2486@gmail.com>
2022-09-06 16:47:47 +0900

    BOJ_11497_이나현

---
+++ 22주차/BOJ_11497/BOJ_11497_이나현.py
@@ -0,0 +1,13 @@
+#[통나무 건너뛰기]
+T = int(input())
+for t in range(T):
+    N = int(input())
+    arr = sorted(list(map(int, input().split())))
+    arr1 = arr[::2]
+    arr2 = arr[1::2]
+    arr1 = arr1[::-1]
+    arr = arr1 + arr2
+    answer = 0
+    for i in range(N):
+        answer = max(answer, abs(arr[i] - arr[i-1]))
+    print(answer)
