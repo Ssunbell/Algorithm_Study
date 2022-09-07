ad1200c559f25b27e5e8427e923cacda81e44123
lee_seunghwan <tmdghks304@gmail.com>
2022-09-08 01:11:29 +0900

    1697 코드 수정

--- 22주차/BOJ_1697/BOJ_1697_이승환.py
+++ 22주차/BOJ_1697/BOJ_1697_이승환.py
@@ -8,8 +8,8 @@ q.append(n)
 
 # 몇번만에 갔는지 확인
 # 100000은 안되고 100001은 통과. 이유는 모르겠다.
-# depth_chart = [0] * 100001
-depth_chart = {n:0}
+# depth_chart = [0] * 100000
+depth_chart = [0] * 100001
 
 while q:
     curr = q.popleft()
@@ -27,7 +27,7 @@ while q:
 
 print(depth_chart[k])
 
-
+# 딕셔너리로 구현, 시간초과. 딕셔너리의 시간복잡도가 높아서 그런듯.
 # from collections import deque
 
 # n, k = map(int, input().split())
