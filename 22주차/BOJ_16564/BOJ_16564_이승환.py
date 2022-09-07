1378313b8a7052f34d377260bcabfed2e52250b4
lee_seunghwan <tmdghks304@gmail.com>
2022-09-07 23:55:13 +0900

    이승환 문제풀이 업로드

--- 22주차/BOJ_16564/BOJ_16564_이승환.py
+++ 22주차/BOJ_16564/BOJ_16564_이승환.py
@@ -1,3 +1,8 @@
 n, k = map(int,input().split())
 t = list(int(input()) for _ in range(n))
 
+start = min(t)
+end = max(t) + k
+
+while True:
+    
---
+++ 22주차/BOJ_1697/BOJ_1697_이승환.py
@@ -0,0 +1,58 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+# 탐색할 큐
+q = deque()
+q.append(n)
+
+# 몇번만에 갔는지 확인
+# 100000은 안되고 100001은 통과. 이유는 모르겠다.
+# depth_chart = [0] * 100001
+depth_chart = {n:0}
+
+while q:
+    curr = q.popleft()
+
+    # 종료지점에 오면 종료
+    if curr == k:
+        break
+
+    # 다음 지점 큐에 저장
+    next_list= [curr-1, curr+1, curr*2]
+    for next in next_list:
+        if 0 <= next <= 100000 and depth_chart[next] == 0:
+            q.append(next)
+            depth_chart[next] = depth_chart[curr] + 1
+
+print(depth_chart[k])
+
+
+# from collections import deque
+
+# n, k = map(int, input().split())
+
+# # 탐색할 큐
+# q = deque()
+# q.append(n)
+
+# # 몇번만에 갔는지 확인
+# depth_chart = {n:0}
+
+# while q:
+#     curr = q.popleft()
+
+#     # 종료지점에 오면 종료
+#     if curr == k:
+#         break
+
+#     # 다음 지점 큐에 저장
+#     next_list= [curr-1, curr+1, curr*2]
+#     for next in next_list:
+#         try :
+#             depth_chart[next]
+#         except:
+#             q.append(next)
+#             depth_chart[next] = depth_chart[curr] + 1
+
+# print(depth_chart[k])
