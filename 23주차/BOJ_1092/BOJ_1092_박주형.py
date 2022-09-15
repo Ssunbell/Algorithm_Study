762bb1002e086c31eb97630b7bda4abd37d2ba07
parkjubro <juhyeong97@gmail.com>
2022-09-15 21:16:16 +0900

    BOJ_1092_박주형

---
+++ 23주차/BOJ_1092/BOJ_1092_박주형.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cranes = list(map(int, input().split()))
+m = int(input())
+boxes = list(map(int, input().split()))
+
+cranes.sort(reverse=True)
+boxes.sort(reverse=True)
+
+time = 0
+if cranes[0] < boxes[0]:
+    print(-1)
+
+while len(boxes)>0:
+    for crane in cranes:
+        for box in boxes:
+            if crane >= box:
+                boxes.remove(box)
+                break
+    time+=1
+
+print(time)
