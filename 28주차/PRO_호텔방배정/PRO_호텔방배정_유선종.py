75ea760b0fb24c910abd3e87466f63dba2516c86
Trailblazer-Yoo <yousun9591@naver.com>
2022-10-23 11:53:17 +0900

    PRO_호텔방배정_유선종

---
+++ 28주차/PRO_호텔방배정/PRO_호텔방배정_유선종.py
@@ -0,0 +1,21 @@
+import sys
+sys.setrecursionlimit(10**8)
+def find_parent(room,n):
+    try:
+        # key: 방 번호, value : 최소 방 번호로 업데이트
+        room[n] = find_parent(room,room[n])
+
+        return room[n]
+    except:
+        # key : 방 번호, value : 그 다음 방 번호
+        room[n] = n + 1
+
+        return n
+
+
+def solution(k, room_number):
+    result = []
+    room = dict()
+    for n in room_number:
+        result.append(find_parent(room,n))
+    return result
