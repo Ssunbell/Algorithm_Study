14515ac4f9f8cbf73c7f29a83b786dc80764936d
KTH <thkyang324@kakao.com>
2023-03-08 14:13:28 +0900

    강태훈 표 병합

--- 35주차/PRO_표병합/PRO_표병합_강태훈.py
+++ 35주차/PRO_표병합/PRO_표병합_강태훈.py
@@ -1,104 +1,88 @@
-class Node:
-    def __init__(self, key=None, data=None, parent=None):
-        self.key = key
-        self.data = data
-        self.parent = parent
+parent = [i for i in range(2500)]
+value = ["EMPTY" for _ in range(2500)]
 
+def find(x):
+    """ x의 부모노드 찾아줌 """
+    if x != parent[x]:
+        ploc = parent[x]
+        parent[x] = find(ploc)
+    return parent[x]
 
-class Table:
-    def __init__(self):
-        self.table = [[Node(key=(x,y), parent=(x,y)) for x in range(50)] for y in range(50)]
-        
-        self.show()
-
-    def get_parent(self, x, y):
-        x, y = map(int, (x,y))
-    #     if not (0<=x<50 and 0<=y<50):
-    #         raise("location out of bound")
-        c_node = self.table[y][x]
-        if c_node.key == c_node.parent:
-            return (x, y)
-        p_node = self.get_parent(*c_node.parent)
-        c_node.parent = p_node
-        return c_node.parent
-
-    def update(self, x, y, data):
-        x, y = map(int, (x,y))
-        px, py = self.get_parent(x, y)
-        self.table[py][px].data = data
-        self.show()
+def union(x1, x2):
+    """ merge """
+    x1, x2 = find(x1), find(x2)
+    if x1 == x2:
+        return
+    parent[x2] = x1
+    val = value[x1] if value[x1]!="EMPTY" else value[x2]
+    update_1(x1, val)
     
-    def update_value(self, value1, value2):
-        for line in self.table:
-            for cell in line:
-                if cell.data == value1:
-                    cell.data = value2
-    
-    def print(self, x, y):
-        x, y = map(int, (x,y))
-        px, py = self.get_parent(x, y)
-        if self.table[py][px].data:
-            return self.table[py][px].data
-        else:
-            return "EMPTY"
-    
-    def merge(self, r1, c1, r2, c2):
-        r1, c1, r2, c2 = map(int, (r1,c1,r2,c2))
-        if r1==r2 and c1==c2:
-            return
-        node_1 = self.table[c1][r1]
-        node_2 = self.table[c2][r2]
-        merge_value = None
-        if bool(node_1.data)^bool(node_2.data):
-            merge_value = node_1.data if node_1.data else node_2.data
-        elif node_2.data and node_1.data:
-            merge_value = node_1.data
-        pnode, cnode = sorted([node_1.key, node_2.key])
-        px, py = self.get_parent(*pnode)
-        self.table[cnode[1]][cnode[0]].parent = (px, py)
-        self.table[py][px].data = merge_value
-        self.show()
-    def unmerge(self, r, c):
-        r, c = map(int, (r,c))
-        px, py = self.get_parent(r, c)
-        self.table[c][r].data = self.table[py][px].data
-        self.table[c][r].parent = (r,c)
+def update_1(x, val):
+    """ x좌표의 value를 val로 업데이트 """
+    head = find(x)
+    for i in range(2500):
+        if find(i) == head:
+            value[i] = val
 
-    def show(self, title=None):
-        print("-"*50)
-        print(f"{title}")
-        for l in self.table:
-            for dot in l:
-                print(dot.data, end="\t")
-            print()
-        print("-"*50)
-        
-    
+def update_2(v1, v2):
+    """ value가 v1인 셀의 value를 v2로 """
+    for i in range(2500):
+        loc = find(i)
+        if value[loc] == v1:
+            value[loc] = v2
 
-    # UPDATE, MERGE, UNMERGE, PRINT
+def unmerge(x):
+    """ unmerge """
+    head = find(x)
+    val = value[head]
+    for i in range(2500):
+        if find(i) == head:
+            parent[i] = i
+            value[i] = "EMPTY" if i != x else value[i]
+
+getloc = lambda x,y : 50*(int(x)-1) + (int(y)-1)
+
+def show(board, title="", x_grid=5, y_grid=5):
+    """ 디버깅용 유틸 함수. x_grid, y_grid만큼 보여줌 """
+    print("-"*20+title+"-"*20)
+    for idx, i in enumerate(range(0,2500,50)):
+        if idx == y_grid: break
+        print(board[i:i+x_grid])
+    print("-"*50)
 
 def solution(commands):
-    pd = Table()
     answer = []
     for command in commands:
-        splited_cmd = command.split()
-        function, value = splited_cmd[0], splited_cmd[1:]
-        if function == "UPDATE":
-            if len(value)==3:
-                pd.update(*value)
-            elif len(value)==2:
-                pd.update_value(*value)
-        elif function == "MERGE":
-            pd.merge(*value)
-        elif function == "UNMERGE":
-            pd.unmerge(*value)
-        elif function == "PRINT":
-            answer.append(pd.print(*value))
+        cmd = command.split()
+        cmd_len = len(cmd)
+        if cmd[0] == "UPDATE":
+            if cmd_len == 3:
+                update_2(*cmd[1:])
+            elif cmd_len == 4:
+                r, c, v = cmd[1:]
+                update_1(getloc(r,c), v)
+                
+        elif cmd[0] == "MERGE":
+            r1, c1, r2, c2 = cmd[1:]
+            loc1, loc2 = getloc(r1,c1), getloc(r2,c2)
+            union(loc1, loc2)
+            
+        elif cmd[0] == "UNMERGE":
+            loc = getloc(*cmd[1:])
+            unmerge(loc)
+                
+        elif cmd[0] == "PRINT":
+            loc = getloc(*cmd[1:])
+            answer.append(value[find(loc)])
+        # print(command)
+        # show(value, title="value")
+        # show(parent, title="parent")
+
     return answer
 
 tc = [
     ["UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"],
-    ["UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"]
+    ["UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"],
 ]
 for c in tc:
     print(solution(c))
