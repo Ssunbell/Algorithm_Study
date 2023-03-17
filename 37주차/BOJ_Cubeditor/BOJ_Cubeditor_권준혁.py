8532b698f261dc61a58879e2a06846dbb3db1395
tree_jhk <tree.jhk@gmail.com>
2023-03-18 03:35:40 +0900

    Cubeditor, 주사위굴리기2 수정완료 권준혁

--- 37주차/BOJ_Cubeditor/BOJ_Cubeditor_권준혁.py
+++ 37주차/BOJ_Cubeditor/BOJ_Cubeditor_권준혁.py
@@ -1,9 +1,17 @@
-s = input()
-for i in range(1, len(s)):
-    for j in range(0, i):
-        tmp = s[j:len(s) - i + j]
-        if tmp in s[j + 1:]:
-            print(len(tmp))
-            exit(0)
-else:
-    print(0)
+def make_table(s):
+    j = 0
+    table = [0] * len(s)
+    for i in range(1, len(s)):
+        while j > 0 and s[i] != s[j]:
+            j = table[j - 1]
+        if s[i] == s[j]:
+            j += 1
+            table[i] = j
+    return table
+
+if __name__ == '__main__':
+    s = input()
+    answer = 0
+    for i in range(len(s)):
+        answer = max(answer, max(make_table(s[i:])))
+    print(answer)
